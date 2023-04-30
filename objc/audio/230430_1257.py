c645232c028a9820b8940628ec6caa5f7da73937
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-04-30 13:13:07 +0900

    audioEngine

---
+++ objc/audio/230430_1257.py
@@ -0,0 +1,35 @@
+from objc_util import ObjCClass
+
+import pdbg
+
+AVAudioEngine = ObjCClass('AVAudioEngine')
+AVAudioSourceNode = ObjCClass('AVAudioSourceNode')
+AVAudioFormat = ObjCClass('AVAudioFormat')
+
+
+class Synth:
+  def __init__(self):
+    self.audioEngine: AVAudioEngine
+    self.sampleRate: float = 44100.0  # set_up メソッド: outputNode より確定
+    self.deltaTime: float = 0.0  # 1/sampleRate 時間間隔
+
+    self.set_up()
+
+  def set_up(self):
+    audioEngine = AVAudioEngine.new()
+    sourceNode = AVAudioSourceNode.alloc()
+    mainMixer = audioEngine.mainMixerNode()
+    outputNode = audioEngine.outputNode()
+    format = outputNode.inputFormatForBus_(0)
+
+    self.sampleRate = format.sampleRate()
+    self.deltaTime = 1 / self.sampleRate
+
+    inputFormat = AVAudioFormat.alloc(
+    ).initWithCommonFormat_sampleRate_channels_interleaved_(
+      format.commonFormat(), self.sampleRate, 1, format.isInterleaved())
+
+
+if __name__ == '__main__':
+  synth = Synth()
+
