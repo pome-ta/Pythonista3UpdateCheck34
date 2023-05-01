28534d87cd96138dcf345ed99f9c5bc353cf7672
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-05-01 19:43:49 +0900

    log

--- objc/blockTest/230501_0116.py
+++ objc/blockTest/230501_0116.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 
 # https://github.com/jsbain/objc_hacks/blob/master/blocktest.py
-
 '''
 experiment with blocks.
 
@@ -14,21 +13,22 @@ from ctypes import *
 from objc_util import *
 import pdb
 
-NSBlock=ObjCClass('NSBlock')
+NSBlock = ObjCClass('NSBlock')
 from ctypes import *
 
-ENUMCALLBACK=CFUNCTYPE(None, c_void_p,c_void_p, c_uint, POINTER(c_bool))
+ENUMCALLBACK = CFUNCTYPE(None, c_void_p, c_void_p, c_uint, POINTER(c_bool))
 
 
 class GenericBlockDescriptor(Structure):
-   _fields_=[('reserved',c_ulong),
-            ('size',c_ulong),
-            ('signature', c_char_p)]
-   def __init__(self,block):
-      self.size=sizeof(block)
+  _fields_ = [('reserved', c_ulong), ('size', c_ulong),
+              ('signature', c_char_p)]
+
+  def __init__(self, block):
+    self.size = sizeof(block)
+
 
 class EnumerationBlock(Structure):
-   '''
+  '''
    struct Block_literal_1 {
     void *isa; // initialized to &_NSConcreteStackBlock or &_NSConcreteGlobalBlock
     int flags;
@@ -45,26 +45,27 @@ class EnumerationBlock(Structure):
     } *descriptor;
     // imported variables
    };'''
-   _fields_=  [('isa',c_void_p),
-               ('flags',c_int32),
-               ('reserved',c_int32),
-               ('invoke',ENUMCALLBACK),
-               ('descriptor',POINTER(GenericBlockDescriptor))]
-   def __init__(self,invoke):        
-      self._descriptor=GenericBlockDescriptor(self)
-      self.descriptor=pointer(self._descriptor)
-      self.isa=NSBlock.ptr
-      self.invoke=ENUMCALLBACK(invoke)
-      self.descriptor[0].signature=c_char_p('') # not used
-      self.descriptor[0].size=sizeof(self)
+  _fields_ = [('isa', c_void_p), ('flags', c_int32), ('reserved', c_int32),
+              ('invoke', ENUMCALLBACK),
+              ('descriptor', POINTER(GenericBlockDescriptor))]
+
+  def __init__(self, invoke):
+    self._descriptor = GenericBlockDescriptor(self)
+    self.descriptor = pointer(self._descriptor)
+    self.isa = NSBlock.ptr
+    self.invoke = ENUMCALLBACK(invoke)
+    #self.descriptor[0].signature = c_char_p('')  # not used
+    self.descriptor[0].size = sizeof(self)
 
 
 def invoke_py(self, obj, ind, stop):
-   print('The item at index',ind,'is', ObjCInstance(obj))
-   if ObjCInstance(obj).intValue()==-1:
-      print('stopping here')
-      stop[0]=True
-      
-blk=EnumerationBlock(invoke_py)
-A=ns([4,3,7,9,-1, 5])
-A.enumerateObjectsUsingBlock_(byref(blk))
+  print('The item at index', ind, 'is', ObjCInstance(obj))
+  if ObjCInstance(obj).intValue() == -1:
+    print('stopping here')
+    stop[0] = True
+
+
+blk = EnumerationBlock(invoke_py)
+A = ns([4, 3, 7, 9, -1, 5])
+A.enumerateObjectsUsingBlock_((blk))
+
---
+++ sysInfo/230501_1935.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib
+import flask
+
+print(f'Numpy: {np.__version__}')
+print(f'matplotlib: {matplotlib.__version__}')
+print(f'flask: {flask.__version__}')
