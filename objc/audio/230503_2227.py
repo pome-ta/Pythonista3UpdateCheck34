7e66f57297b6fe6cac04ff6ed8bd54ebb5bd8f5b
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-05-04 16:01:36 +0900

    not work

--- objc/audio/230503_2227.py
+++ objc/audio/230503_2227.py
@@ -23,6 +23,41 @@ class AudioTimeStampFlags(ctypes.c_uint32):
                                         | kAudioTimeStampHostTimeValid)
 
 
+class SMPTETimeType(ctypes.c_uint32):
+  kSMPTETimeType24 = 0
+  kSMPTETimeType25 = 1
+  kSMPTETimeType30Drop = 2
+  kSMPTETimeType30 = 3
+  kSMPTETimeType2997 = 4
+  kSMPTETimeType2997Drop = 5
+  kSMPTETimeType60 = 6
+  kSMPTETimeType5994 = 7
+  kSMPTETimeType60Drop = 8
+  kSMPTETimeType5994Drop = 9
+  kSMPTETimeType50 = 10
+  kSMPTETimeType2398 = 11
+
+
+class SMPTETimeFlags(ctypes.c_uint32):
+  kSMPTETimeUnknown = 0
+  kSMPTETimeValid = (1 << 0)
+  kSMPTETimeRunning = (1 << 1)
+
+
+class SMPTETime(ctypes.Structure):
+  _fields_ = [
+    ('mSubframes', ctypes.c_int16),
+    ('mSubframeDivisor', ctypes.c_int16),
+    ('mCounter', ctypes.c_uint32),
+    ('mType', SMPTETimeType),
+    ('mFlags', SMPTETimeFlags),
+    ('mHours', ctypes.c_int16),
+    ('mMinutes', ctypes.c_int16),
+    ('mSeconds', ctypes.c_int16),
+    ('mFrames', ctypes.c_int16),
+  ]
+
+
 class AudioTimeStamp(ctypes.Structure):
   _fields_ = [
     ('mFlags', AudioTimeStampFlags),
@@ -50,20 +85,33 @@ class AudioBufferList(ctypes.Structure):
   ]
 
 
+def source_node_render(_cmd, _isSilence_ptr, _timestamp_ptr, frameCount,
+                       outputData_ptr) -> OSStatus:
+  # todo: ここに処理を書く
+  print('test')
+  return 1
+
+
+render_block = ObjCBlock(
+  source_node_render,
+  restype=OSStatus,
+  argtypes=[
+    ctypes.c_void_p,  # _cmd
+    ctypes.POINTER(ctypes.c_bool),  # isSilence
+    ctypes.POINTER(AudioTimeStamp),  # timestamp
+    ctypes.c_uint32,  # frameCount
+    ctypes.POINTER(AudioBufferList),  # outputData
+  ])
+
+
+
+
 class Synth:
 
   def __init__(self):
     self.audioEngine: AVAudioEngine
     self.sampleRate: float = 44100.0  # set_up メソッド: outputNode より確定
     self.deltaTime: float = 0.0  # 1/sampleRate 時間間隔
-
-    self.render_block = ObjCBlock(self.source_node_render,
-                                  restype=OSStatus,
-                                  argtypes=[
-                                    ctypes.c_void_p, ctypes.c_void_p,
-                                    ctypes.c_void_p, ctypes.c_void_p,
-                                    ctypes.POINTER(AudioBufferList)
-                                  ])
 
     self.set_up()
 
@@ -81,7 +129,8 @@ class Synth:
     ).initWithCommonFormat_sampleRate_channels_interleaved_(
       format.commonFormat(), self.sampleRate, CHANNEL, format.isInterleaved())
 
-    sourceNode.initWithFormat_renderBlock_(inputFormat, self.render_block)
+    #sourceNode.initWithFormat_renderBlock_(inputFormat, self.render_block)
+    sourceNode.initWithFormat_renderBlock_(inputFormat, render_block)
     audioEngine.attachNode_(sourceNode)
     sourceNode.volume = 0.2
 
@@ -90,16 +139,11 @@ class Synth:
 
     audioEngine.prepare()
     self.audioEngine = audioEngine
-    pdbg.state(audioEngine.attachedNodes())
-
-  def source_node_render(self, _cmd, _isSilence_ptr, _timestamp_ptr,
-                         frameCount, outputData_ptr) -> OSStatus:
-    # todo: ここに処理を書く
-    print('test')
-    return 0
 
   def start(self):
-    self.audioEngine.startAndReturnError_(None)
+    print('a')
+    b = self.audioEngine.startAndReturnError_(None)
+    print(b)
 
   def stop(self):
     self.audioEngine.stop()
@@ -107,5 +151,5 @@ class Synth:
 
 if __name__ == '__main__':
   synth = Synth()
-  #synth.start()
+  synth.start()
 
