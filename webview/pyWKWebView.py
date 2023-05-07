ae60b129ac808e25fe8d6c150b0f989e4c9b05a1
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-05-08 02:15:54 +0900

    WKWebView

---
+++ webview/pyWKWebView.py
@@ -0,0 +1,16 @@
+import ctypes
+
+from objc_util import ObjCClass
+import ui
+
+import pdbg
+
+WKWebView = ObjCClass('WKWebView')
+pdbg.state(WKWebView)
+
+
+class PyWKWebView():
+
+  def __init__():
+    pass
+
